424244eb47e035ed0a5203b029e68233aebb45c0
abdiarale86 <a.arale86@gmail.com>
2022-12-31 23:32:22 -0500

    Guess number  game

---
+++ Guess_number.py
@@ -0,0 +1,17 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while  guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry, you have to guess again, you number is too low.')
+
+        elif guess > random_number:
+            print('Sorry, guess again. Your number is too high.')
+
+    print(f'Yay, perfect guess. you have guess the right number {random_number} correctly')
+    
+    
+guess(10)
